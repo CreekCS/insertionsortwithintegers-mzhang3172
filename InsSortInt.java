51811d8b0123c1fa3c5fec8bf79b857bbc6cf7ba
mzhang3172 <mzhang3@cherrycreekschools.org>
2024-08-28 19:40:34 +0000

    hi2

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,10 +1,42 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class InsSortInt {
+
     public static void main(String[] args) {
-        int[] array = {10,9,8,7,6,5,4,3,2,1};
-        insertionSort(array);
-        for (int i : array) {
-            System.out.print(i + " ");
+        int[] tall = new int[1000];
+
+        try {
+      
+            tall = loadNumbersFromFile("randInts.txt");
+
+            insertionSort(tall);
+
+            System.out.println("Smallest number: " + tall[0]);
+            System.out.println("Largest number: " + tall[tall.length - 1]);
+
+        } catch (IOException e) {
+            System.err.println("Error reading the file: " + e.getMessage());
         }
+    }
+
+    public static int[] loadNumbersFromFile(String fileName) throws IOException {
+        int[] numbers = new int[1000];
+        File file = new File(fileName);
+        FileReader inputFile = new FileReader(file);
+        BufferedReader in = new BufferedReader(inputFile);
+
+        String s;
+        int i = 0;
+        while ((s = in.readLine()) != null && i < 1000) {
+            numbers[i] = Integer.parseInt(s);
+            i++;
+        }
+
+        in.close();
+        return numbers;
     }
 
     public static void insertionSort(int[] array) {
