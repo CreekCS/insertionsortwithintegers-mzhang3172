9b85fa6547c6024b052a03f27228583a988f78f3
mzhang3172 <mzhang3@cherrycreekschools.org>
2024-08-28 01:34:46 +0000

    hi

--- InsSortInt.java
+++ InsSortInt.java
@@ -1,3 +1,21 @@
 public class InsSortInt {
+    public static void main(String[] args) {
+        int[] array = {10,9,8,7,6,5,4,3,2,1};
+        insertionSort(array);
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+    }
 
+    public static void insertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
 }
